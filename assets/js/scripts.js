43b16823db491be87457d4cd9198f28d134284d5
Arin <arinarjani@gmail.com>
2018-06-27 11:44:18 -0700

    Added some copy for the first page and tried to make a layout

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+(() => {
+  // grab the .navArrow from the html
+  let navArrow = document.querySelectorAll('.navArrow');
+  // grab the navArrow styles for less typing later on
+  let navArrowS = navArrow.styles;
+  // grab the
+
+
+})();
