09b0f9594b868886080e0a200c9c275cb3802cd7
Arin <arinarjani@gmail.com>
2018-06-29 14:28:26 -0700

    FINALLY got the drop down menu to work via js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,41 @@
 (() => {
-  // grab the .navArrow from the html
+  // grab the classes .navArrow, .rotate, and .insideUL from HTML
   let navArrow = document.querySelectorAll('.navArrow');
-  // grab the navArrow styles for less typing later on
-  let navArrowS = navArrow.styles;
-  // grab the
+  // let rotate = document.querySelectorAll('.rotate');
+  // let insideUL = document.querySelectorAll('.insideUL');
+  // cache navArrow.length for speed
+  let navArrowLength = navArrow.length;
+  // keep track of the user clicks for the navArrow btns
+  clicked = false;
+  // logic for first click ( open ):
+  // - click the navArrow
+  // - click var turns to true
+  // - arrow rotates 90 degrees
+  // - ul shows
+  //
+  // logic for second click ( close ):
+  //  - click var turns to false
+  //  - arrow rotates to 0 degrees
+  //  - ul hides
 
-
+  // loop through each navArrow btn and select which one the user clicks
+  for (let i = 0; i < navArrowLength; i++) {
+    navArrow[i].addEventListener('click', () => {
+      if ( clicked == false ) {
+        // first click
+        let rotate = document.querySelectorAll('.rotate');
+        let insideUL = document.querySelectorAll('.insideUL');
+        clicked = true;
+        rotate[i].style.transform = 'rotate(90deg)';
+        insideUL[i].style.display = 'block';
+      } else {
+        // second click
+        let rotate = document.querySelectorAll('.rotate');
+        let insideUL = document.querySelectorAll('.insideUL');
+        clicked = false;
+        rotate[i].style.transform = 'rotate(0deg)';
+        insideUL[i].style.display = 'none';
+      }
+    })
+  }
 })();
