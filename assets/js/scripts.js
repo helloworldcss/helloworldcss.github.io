7be5725f95cb0d070607a09af48406f1f2e8c5e3
helloworldcss <helloworldcsstuts@gmail.com>
2018-07-03 16:37:00 -0700

    Changed a little bit of everything
    JS:
    - changed .rotate, .navArrow, and insideUL styles for open and closing buttons
    
    CSS:
    - added lots of styles to just about every item on the page
    
    HTML:
    - saw some cool tricks using <span>'s to get each element to have it's own width

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,17 +9,21 @@
 
   // keep track of the user clicks for the navArrow btns
   clicked = false;
-  
+
   // logic for first click ( open ):
   // - click the navArrow
   // - click var turns to true
   // - arrow rotates 90 degrees
   // - ul shows
+  // - remove class slideInDown
+  // - add class slideInUp
   //
   // logic for second click ( close ):
   //  - click var turns to false
   //  - arrow rotates to 0 degrees
   //  - ul hides
+  // - remove class slideInUp
+  // - add class slideInDown
 
   // loop through each navArrow btn and select which one the user clicks
   for (let i = 0; i < navArrowLength; i++) {
@@ -30,14 +34,22 @@
         let insideUL = document.querySelectorAll('.insideUL');
         clicked = true;
         rotate[i].style.transform = 'rotate(90deg)';
-        insideUL[i].style.display = 'block';
+        rotate[i].style.verticalAlign = 'middle';
+        navArrow[i].style.verticalAlign = 'sub';
+        insideUL[i].style.display = 'flex';
+        insideUL[i].style.flexDirection = 'column';
+        // insideUL[i].classList.remove('slideInUp');
+        // insideUL[i].classList.add('slideOutDown');
       } else {
         // second click
         let rotate = document.querySelectorAll('.rotate');
         let insideUL = document.querySelectorAll('.insideUL');
         clicked = false;
         rotate[i].style.transform = 'rotate(0deg)';
+        rotate[i].style.verticalAlign = 'sub';
         insideUL[i].style.display = 'none';
+        // insideUL[i].classList.remove('slideInDown');
+        // insideUL[i].classList.add('slideOutUp');
       }
     })
   }
